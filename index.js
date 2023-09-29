c9c7e99affe7e44a3ba2ebd010d18ef904f5c1b0
jampierzcode <jampierv127@gmail.com>
2023-09-29 14:42:56 -0500

    v1 server chat gpt

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const { OpenAI } = require("openai");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY_2,
+});
+
+app.post("/botapi", async (req, res) => {
+  try {
+    const messages = req.body.messages;
+
+    // Llama a la API de ChatGPT utilizando la librería openai
+    const chatGptResponse = await openai.chat.completions.create({
+      messages,
+      model: "gpt-3.5-turbo",
+    });
+
+    // Devuelve la respuesta de ChatGPT como JSON
+    res.json({ response: chatGptResponse.choices[0].message.content });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
