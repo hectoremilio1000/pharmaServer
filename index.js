930113adc9095988265ed3bb31d24b1d31590a9d
jampierzcode <jampierv127@gmail.com>
2023-09-29 15:19:28 -0500

    change port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const { OpenAI } = require("openai");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.use(bodyParser.json());
 
